2d936027690ebb303431e2faa7db3252a89d254c
jarquinsandra <jarquinsandra@gmail.com>
2020-08-03 14:23:13 -0500

    Update __init__.py
    change mysql password to root

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app():
     #app.config.from_object(settings_module)
     #app.config['APP_SETTINGS_MODULE']="config.prod"
     #Here you need to define the URI database, with the following data 'mysql+pymysql:user:password@host:port/database' you can change the data according to your needs
-    app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:askl@localhost:3306/consensus'
+    app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost:3306/consensus'
     app.config['SECRET_KEY'] = '5e04a4955d8878191923e86fe6a0dfb24edb226c87d6c7787f35ba4698afc86e95cae409aebd47f7'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
